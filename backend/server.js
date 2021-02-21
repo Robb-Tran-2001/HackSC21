ee8bef1e2762fd648fcf6bedb47585ce74a9103e
Robb Tran <bobtran@usc.edu>
2021-02-21 06:44:02 -0800

    Last from me

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose') //mongodb
 require('dotenv').config({path: __dirname + '/.env'}); //dotenv config
 const app = express() //init app function
 
+// Imports the Google Cloud client library
+const {Translate} = require('@google-cloud/translate').v2;
+
+// Creates a client
+const translate = new Translate();
+
 const server = require('http').createServer(app) //supply to http server
 const io = require('socket.io')(server)
 
@@ -28,3 +34,23 @@ const users = {};
 server.listen(port, () => {
   console.log('Server running on port ', port)
 })
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+// const text = 'The text to translate, e.g. Hello, world!';
+// const target = 'The target language, e.g. ru';
+
+async function translateText() {
+  // Translates the text into the target language. "text" can be a string for
+  // translating a single piece of text, or an array of strings for translating
+  // multiple texts.
+  let [translations] = await translate.translate("I ate dog food", 'Chinese');
+  translations = Array.isArray(translations) ? translations : [translations];
+  console.log('Translations:');
+  translations.forEach((translation, i) => {
+    console.log(`${text[i]} => (${target}) ${translation}`);
+  });
+}
+
+translateText();
